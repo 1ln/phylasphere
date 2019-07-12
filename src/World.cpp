5bd7c0bca14432ce01782ee62c6a14f0bd25f848
Dan Olson <dano7900@gmail.com>
2019-07-12 19:55:36 -0400

    update color

--- src/World.cpp
+++ src/World.cpp
@@ -18,36 +18,45 @@ void World::init() {
     _r = ofRandom(0,4000000);
     _scale = 100;
     _map_width = 256;
-    _tile_width = 8;
+    _tile_width = 4;
     _steps = _map_width / _tile_width;
     _total_tiles = _steps * _steps;
-    _ambient_lighting.setAmbientColor(ofColor(0,0,0));
-    
-    //tileType(_r,_r);
-    //box.set(_tile_width,_tile_width,_elevation);
-    //box.setPosition(0,0,0);
-    //tile.push_back(box);
+    //_ambient_lighting.setAmbientColor(ofColor(0,0,0));
+    //light.setAttenuation(0.001,0.0001,0.001);
+    //material.setEmissiveColor(ofColor(255,255,255));
 
 }
 
 void World::scale(float s) {
+
 _scale = s;
+
+}
+
+void World::sideColor() {
+
+for(int i = 0; i < 6; ++i) {
+box.setSideColor(i,_c);
+}
+
 }
 
 void World::tileType(float i,float j) {
 _n = noise.octave(8,i*_scale*.00001,j*_scale*.00001,.5,2);
 if(_n < .48) {
 
-_elevation = 0;
-_c.set(0,5,75*_n,155);
+_elevation = 10;
+_c.set(0,5,75*_n,255);
+//material.setEmissiveColor(ofColor(0,5,75*_n,255));
+
 _light = false;
 }
-    if(_n > .479 && _n < .48) {
+    if(_n > .48 && _n < .49) {
     _elevation = _n*_scale;
     _c.set(_n*15,_n*15,_n*15,255);
     _light = true;
 }
-    if(_n > .51) {
+    if(_n > .49) {
     _c.set(_n*15,_n*15,_n*15,255);
     _elevation =  _n * _scale;
     _light = false;     
@@ -64,17 +73,39 @@ init();
 }
 
 void World::remap() {
-
 if(tile.size() >= _total_tiles) {
-tile.erase(tile.begin(),tile.begin()+32);
+materials.erase(materials.begin(),materials.begin()+_steps);
+tile.erase(tile.begin(),tile.begin()+_steps);
+}
+
+if(lights.size() >= 1128) {
+lights.erase(lights.begin(),lights.begin()+1);
 }
 
 _x_width += _tile_width;
+
 for(int i = 0; i < _map_width-1; i+= _tile_width) {
+
     tileType(_x_width+_r,i+_r);
+    
+    material.setEmissiveColor(ofColor(5,5,5));
+    materials.push_back(material);
+  
     box.set(_tile_width,_tile_width,_elevation);
     box.setPosition(_x_width,i,0);
+    //sideColor();
+    //material.end();
     tile.push_back(box);
+
+    if(_light == true) {
+    light.setPointLight();
+    light.setPosition(_x_width,i,_elevation+5);
+    light.setAttenuation(0.0001,0.001,0.001);
+    //light.enable();
+
+    lights.push_back(light);
+    }
+
 }
 
 }
@@ -92,9 +123,23 @@ world_plane.rotateDeg(y,0,0,1);
 }
 
 void World::draw() {
+
 for(unsigned int i = 0; i < tile.size(); ++i) {
+
+//material.begin();
+materials[i].begin();
 tile[i].draw();
+materials[i].end();
+//material.end();
+
+for(unsigned int i = 0; i < lights.size(); ++i) {
+//lights[i].enable();
 }
+
+}
+
+
+
 
 //ofDrawBox(tile[128+8].X(),tile[128+8].Y(),100,5,5,5);
 //ofDrawBox(0,0,125,5,5,5);
@@ -106,10 +151,10 @@ void World::update() {
 //cout << tile.size() << endl;
 remap();
 
-if(tile.size() >= 1024) {
+if(tile.size() >= _total_tiles) {
 world_plane.setPosition(tile[((_total_tiles-_steps)/2)].getX(),tile[((_total_tiles-_steps)/2)].getY(),0);
-_ambient_lighting.setPosition(tile[((_total_tiles-_steps)/2)].getX(),tile[((_total_tiles-_steps)/2)].getY(),2500);
-_ambient_lighting.enable();
+//_ambient_lighting.setPosition(tile[((_total_tiles-_steps)/2)].getX(),tile[((_total_tiles-_steps)/2)].getY(),2500);
+//_ambient_lighting.enable();
 
 } else {
 world_plane.setPosition(_map_width/2,_map_width/2,0);
